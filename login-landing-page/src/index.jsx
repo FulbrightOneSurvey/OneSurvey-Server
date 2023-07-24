8dbe5279e945a1f2a1250120ad4845647a7cb7ec
jinodk <jinodk2003@gmail.com>
2023-07-24 19:23:45 +0700

    Remove stuff

--- login-landing-page/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { LandingPageLogin } from "./screens/LandingPageLogin";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<LandingPageLogin />);
