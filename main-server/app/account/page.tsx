22c77ee45d682161cc4ca95eeb3bcd8f74643eb2
tnganduong <thanhngan2332@gmail.com>
2023-07-30 14:44:08 +0700

    add account setup page

--- main-server/app/account/page.tsx
+++ main-server/app/account/page.tsx
@@ -5,7 +5,7 @@ import {
     MDBBtn,
     MDBContainer,
     MDBCard, MDBCardBody,
-    MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem
+    MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem,
 
 } from 'mdb-react-ui-kit';
 
@@ -111,6 +111,85 @@ export default function AccountPage() {
                 </div>
                 
             </div>
+            {/* Account info */}
+            
+            <div className='container-fluid' style={{ backgroundColor: '#102064', position: 'relative', color: '#FEA200'}}>
+                <hr className="mx"></hr>
+                <h2 className='d-flex justify-content-center fs-1 fw-bold'>Setup your account here</h2>
+                <hr className="mx"></hr>
+            </div>
+        
+            <div className="container py-5 h-100">
+                <div className='row d-flex justify-content-center align-items-center h-100'>
+                    <div className='card' style={{backgroundColor: '#CEEDF6', borderRadius: '20px'}}>
+                        <div className='card-body'>
+                            <div className='row align-items-center pt-4 pb-3'>
+                                <div className='col-md-3 ps-5'>
+                                    <h6 className="mb-0 fs-3 fw-bold">Full name</h6>
+                                </div>
+                                <div className="col-md-9 pe-5">
+                                    <input type="text" className="form-control form-control-lg" />
+                                </div>
+                            </div>
+                            <hr className="mx-n3"></hr>
+
+                            <div className='row align-items-center py-3'>
+                                <div className='col-md-3 ps-5'>
+                                    <h6 className="mb-0 fs-3 fw-bold">Email</h6>
+                                </div>
+                                <div className="col-md-9 pe-5">
+                                    <input type="email" className="form-control form-control-lg" placeholder='xxx@fulbright.edu.vn' required/>
+                                </div>
+                            </div>
+                            <hr className="mx-n3"></hr>
+
+                            <div className='row align-items-center py-3'>
+                                <div className='col-md-3 ps-5'>
+                                    <h6 className="mb-0 fs-3 fw-bold">Contact info</h6>
+                                </div>
+                                <div className="col-md-9 pe-5">
+                                    <input type="tel" className="form-control form-control-lg" placeholder='(+84)' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+                                </div>
+                            </div>
+                            <hr className="mx-n3"></hr>
+
+                            <div className='row align-items-center py-3'>
+                                <div className='col-md-3 ps-5'>
+                                    <h6 className="mb-0 fs-3 fw-bold">Birthday</h6>
+                                </div>
+                                <div className="col-md-9 pe-5">
+                                    <input type="date" className="form-control form-control-lg" placeholder='(+84)'/>
+                                </div>
+                            </div>
+                            <hr className="mx-n3"></hr>
+                            
+                            <div className='row align-items-center py-3'>
+                                <div className='col-md-3 ps-5'>
+                                    <h6 className="mb-0 fs-3 fw-bold">School</h6>
+                                </div>
+                                <div className="col-md-9 pe-5">
+                                    <select className="select form-control form-control-lg">
+                                        <option value="1" disabled>Choose your university</option>
+                                        <option value="2">Fulbright University Vietnam</option>
+                                        <option value="3">RMIT University Vietnam</option>
+                                        <option value="4">Vietnam National University</option>
+                                        <option value="4">Other</option>
+                                    </select>
+                                </div>
+                            </div>                            
+                            <div className='d-flex justify-content-end px-5 py-2 pt-3'>
+                                <button type="reset" className="btn btn-lg" style={{backgroundColor: '#FEA200', color: '#102064'}}>Reset all</button>
+                                <button type="submit" className="btn btn-lg ms-3" style={{backgroundColor: '#102064', color: '#FEA200'}}>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div>
+                
+                
+            </div>
+
         </div>
     )
 }
@@ -142,5 +221,6 @@ export default function AccountPage() {
 //         </div>
 //     )
 // }
+
 
 
